773718235664b2315227da7e5e62f71b795a9054
Sayan Adhikari <sayanadhikari207@gmail.com>
2021-03-24 21:04:03 +0100

    Parallel implementation

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ import argparse
 ## User defined functions
 from initial import initial
 from thermostat import berendsen
-from pusher import verlet
+# from pusher_serial import verlet
 import diagn
 
 
@@ -61,8 +61,17 @@ def main(argv):
     #========= Diagnostics =======
     dumpPeriod  = int(params['diagnostics']['dumpPeriod'])
     path        = "data/"  # DO NOT CHANGE THE PATH
-    dump        = True
+    dumpData    = bool(params['diagnostics']['dumpData'])
     f           = h5py.File(path+"particle.hdf5","w")
+
+    #========== Options ============
+    parallelMode    = bool(params['options']['parallelMode'])
+    if parallelMode:
+        from pusher_parallel import verlet
+        print("Running in Parallel Mode")
+    else:
+        from pusher_serial import verlet
+        print("Running in Serial Mode")
     #========= Initialize ========
     x,y,z,vx,vy,vz,ux,uy,uz,ax,ay,az,time,data_num = initial(Lx,Ly,Lz,Vxmax,Vymax,Vzmax,N,tmax,Nt,dumpPeriod)
 
@@ -72,7 +81,7 @@ def main(argv):
         KE = 0.0   # Reset KE
         x,y,z,vx,vy,vz,ux,uy,uz,ax,ay,az,KE = verlet(x,y,z,vx,vy,vz,ux,uy,uz,ax,ay,az,dt,Lx,Ly,Lz,N,KE)
         #============ Diagnostics Write ===================
-        if dump:
+        if dumpData:
             if t%dumpPeriod==0:
                 diagn.configSpace(t,N,tmax,x,y,z,Lx,Ly,Lz,f,dt,dumpPeriod)
                 print('TimeSteps = %d'%int(t)+' of %d'%Nt)
