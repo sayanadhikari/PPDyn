45421b222a06e2c6043ff6602dd6f1ef3ee436bb
Sayan Adhikari <sayanadhikari207@gmail.com>
2021-06-25 14:57:19 +0200

    conda environment and slurm script for ppdyn

---
+++ run_slurm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+###############################################
+# Script example for a normal MPI job on Fram #
+###############################################
+
+#SBATCH --job-name=ppdyn01
+#SBATCH --account=nn9299k
+#SBATCH --time=0-00:30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=30
+
+set -o errexit # Exit the script on any error
+set -o nounset # Treat any unset variables as an error
+
+module --quiet purge
+module load Anaconda3/2019.07
+module list
+
+conda env create -f environment.yml
+conda activate ppdyn  
+
+# pip install -r requirements.txt --user
+
+srun python src/main.py -i input.ini
