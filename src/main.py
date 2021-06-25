b3898d10e3175d4fe57fb8196680871b76edc3bd
Sayan Adhikari <sayanadhikari207@gmail.com>
2021-06-25 16:03:53 +0200

    data directory automation

--- src/main.py
+++ src/main.py
@@ -63,6 +63,8 @@ def main(argv):
     #========= Diagnostics =======
     dumpPeriod  = int(params['diagnostics']['dumpPeriod'])
     path        = "data/"  # DO NOT CHANGE THE PATH
+    os.makedirs(path, exist_ok=True)
+        
     dumpData    = bool(params['diagnostics']['dumpData'])
     f           = h5py.File(path+"particle.hdf5","w")
     if dumpData:
