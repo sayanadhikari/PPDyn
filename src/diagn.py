1e0cb4368b02951fa0a73c19c0a009b7d4f75c07
sayanadhikari <sayanadhikari207@gmail.com>
2022-04-11 23:10:35 +0200

    refactoration of code

--- src/diagn.py
+++ src/diagn.py
@@ -1,13 +1,15 @@
 import numpy as np
 from os.path import join as pjoin
-def configSpace(f,dsetE,dsetQ,t,x,y,z,vx,vy,vz,KE,Qcollect,path):
+import config
 
-    f["/%d"%int(t)+"/position/x"] = x
-    f["/%d"%int(t)+"/position/y"] = y
-    f["/%d"%int(t)+"/position/z"] = z
-    f["/%d"%int(t)+"/velocity/vx"] = vx
-    f["/%d"%int(t)+"/velocity/vy"] = vy
-    f["/%d"%int(t)+"/velocity/vz"] = vz
+def configSpace(f,dsetE,dsetQ,t,pos,vvel,KE,Qcollect):
+
+    f["/%d"%int(t)+"/position/x"] = pos[:,0]
+    f["/%d"%int(t)+"/position/y"] = pos[:,1]
+    f["/%d"%int(t)+"/position/z"] = pos[:,2]
+    f["/%d"%int(t)+"/velocity/vx"] = vvel[:,0]
+    f["/%d"%int(t)+"/velocity/vy"] = vvel[:,1]
+    f["/%d"%int(t)+"/velocity/vz"] = vvel[:,2]
     # f["/%d"%int(t)+"/energy"] = KE
 
     dsetE.resize(dsetE.shape[0]+1, axis=0)
@@ -16,20 +18,20 @@ def configSpace(f,dsetE,dsetQ,t,x,y,z,vx,vy,vz,KE,Qcollect,path):
     dsetQ.resize(dsetQ.shape[0]+1, axis=0)
     dsetQ[-1:] = Qcollect
 
-    with open(pjoin(path,'energy.txt'),"ab") as f:
+    with open(pjoin(config.dataDir,'energy.txt'),"ab") as f:
         np.savetxt(f, np.column_stack([t, KE]))
 
     return 0
 
-def attributes(f,tmax,Lx,Ly,Lz,N,dt,dumpPeriod):
-    f.attrs["dp"] = dumpPeriod
-    f.attrs["dt"] = dt
-    f.attrs["Nt"] = int(tmax/(dt*dumpPeriod))
-    f.attrs["N"]  = N
-    f.attrs["tmax"] = tmax
-    f.attrs["Lx"] = Lx
-    f.attrs["Ly"] = Ly
-    f.attrs["Lz"] = Lz
+def attributes(f):
+    f.attrs["dp"] = config.dumpPeriod
+    f.attrs["dt"] = config.dt
+    f.attrs["Nt"] = int(config.tmax/(config.dt*config.dumpPeriod))
+    f.attrs["N"]  = config.N
+    f.attrs["tmax"] = config.tmax
+    f.attrs["Lx"] = config.Lx
+    f.attrs["Ly"] = config.Ly
+    f.attrs["Lz"] = config.Lz
     return 0
 
 def dustDiagn(f,fduration):
