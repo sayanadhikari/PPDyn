ee2e8ecee5dd1f73324c3969e2e2fae1c2e7f266
sayanadhikari <sayanadhikari207@gmail.com>
2022-09-24 10:20:08 +0200

    clean up

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@
 
 @file                main.py
 @authors             Sayan Adhikari <sayan.adhikari@fys.uio.no>
-                     Rupak Mukherjee <rupakm@princeton.edu>
+
 @date                17.03.2021
 
 """
@@ -34,36 +34,6 @@ def main():
     """ PPDyn main() function """
     timer = TaskTimer()
     timer.task('Step: Reading Variables')
-
-    # parser = argparse.ArgumentParser(description='Plasma Particle Dynamics (PPDyn)')
-    # parser.add_argument('-i','--input', default='input.ini', type=str, help='Input file name')
-    # args        = parser.parse_args()
-    # inputFile   = args.input
-    #
-    # params = ini.parse(open(inputFile).read())
-    # #========== Input Parameters ===========
-    #
-    # Lx      = float(params['simbox']['Lx'])  # System length in X
-    # Ly      = float(params['simbox']['Ly'])   # System length in Y
-    # Lz      = float(params['simbox']['Lz'])   # System length in Z
-    #
-    # N       = int(params['particles']['N'])    # Number of particles
-    #
-    # Vxmax   = float(params['particles']['Vxmax']) # Maximum velocity in X
-    # Vymax   = float(params['particles']['Vymax']) # Maximum velocity in Y
-    # Vzmax   = float(params['particles']['Vzmax']) # Maximum velocity in Z
-    #
-    # k       = float(params['screening']['k'])
-    #
-    # g       = float(params['gravity']['g_0'])
-    # #rc      = float(params['cutoff radius']['r_c'])
-    # Temp    = float(params['particles']['Temp'])
-    #
-    # tmax    = float(params['time']['tmax'])  # Final time
-    # dt      = float(params['time']['dt']) # time step size
-    # Nt      = round(tmax/dt) #number of time steps
-    #
-    # dist    = bool(params['particles']['dist'])
 
     #========= Charge and Mass distribution ========
     if config.dist:
